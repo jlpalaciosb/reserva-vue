cacf79722285cdea3ee4e99144649aea145c9ad8
Jose Palacios <joselupalaciosbo@gmail.com>
2022-07-02 16:50:51 -0400

    heroku

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, x-auth-token, content-type');
+    res.setHeader('Access-Control-Allow-Credentials', false);
+    next();
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
